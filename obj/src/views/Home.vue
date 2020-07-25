5f5f4c9701142157c966be0c457696e94ff751eb
张瑜 <1033658198@qq.com>
2020-07-25 20:15:49 +0800

    first commit

---
+++ obj/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <keep-alive exclude="Home"> <input  -->
+    <type="text" v-model="iptValue" v-if="isIptShow">
+    <!-- </keep-alive> -->
+   
+    <button @click="isIptShow=!isIptShow">按钮</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      isIptShow:true,
+      iptValue:""
+    }
+  }
+}
+</script>
